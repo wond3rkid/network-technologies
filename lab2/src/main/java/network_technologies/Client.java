2818df5189db87c21fa4b2e591e11a38003a8d74
savilovaae <a.savilova@g.nsu.ru>
2024-09-27 20:31:03 +0700

    add run

--- lab2/src/main/java/network_technologies/Client.java
+++ lab2/src/main/java/network_technologies/Client.java
@@ -14,37 +14,31 @@ public class Client implements Runnable {
     private final String serverIp;
     private final int serverPort;
     private final String path;
-    private Selector selector;
 
     public Client(String serverIp, int serverPort, String path) {
         this.serverIp = serverIp;
         this.serverPort = serverPort;
         this.path = path;
-        try {
-            selector = Selector.open();
-        } catch (IOException e) {
-            LOGGER.error("Error to open selector");
-        }
     }
 
     @Override
     public void run() {
         try {
             // TODO infinity cycle
-            while (true) {
-                selector.select();
-            }
-        } catch (IOException e) {
-            LOGGER.error("Failed with selector", e);
+        } catch (Exception e) {
+            LOGGER.error(e);
         }
-
     }
+
     // TODO hash map ???
     private void connectToServer() throws IOException {
         SocketChannel socketChannel = SocketChannel.open();
         socketChannel.connect(new InetSocketAddress(serverIp, serverPort));
         socketChannel.configureBlocking(false);
-        socketChannel.register(selector, SelectionKey.OP_CONNECT);
+    }
+
+    private void finishServerConnection() throws IOException {
+
     }
 
 }
